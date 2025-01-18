ac87a496cb80c58c6debfc48ea4c98d37bf1e72a
mihacho <bimnyu9@gmail.com>
2025-01-19 01:58:44 +0900

    Add files via upload

---
+++ assets/shared/stages/whitty.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	makeLuaSprite('whittyBack','whittyBack',-550,-125)
+	addLuaSprite('whittyBack',false)
+	makeLuaSprite('whittyFront','whittyFront',-500,700)
+	addLuaSprite('whittyFront',false)
+end
